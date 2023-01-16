7b71c88cf8bc69eda391064b363796b6932e40d3
Maksim Kazakov <info@kazakovms.ru>
2023-01-16 22:19:58 +0500

    Выполнил полностью ОБЯЗАТЕЛЬНОЕ задание

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let money=150000;
-let income='freelance';
-let addExpenses='Internet, taxi, fuel';
-let deposit=true;
-let mission=5000000;
-let period=12;
+let money = 150000;
+let income = "freelance";
+let addExpenses = "Internet, taxi, fuel";
+let deposit = true;
+let mission = 5000000;
+let period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Перид равен " + period);
+console.log("Цель заработать " + mission + " рублей");
+
+let budgetDay=money/30
+console.log(budgetDay)
